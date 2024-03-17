6593bd026610af30aba1d68adb05c9b7a597a593
Juan Manuel Espinosa Moral <juan.espinosa@eb2.tech>
2024-03-17 13:05:58 +0100

    Utilidad de repositorios locales.

--- git_utils/pull_full.sh
+++ git_utils/pull_full.sh
@@ -1,11 +1,25 @@
 #! /bin/bash
 
-REP_LIST=$(ls "$HOME/Repositorios")
+if [[ $# -ne 1 ]]
+then
+	echo "Modo de uso:"
+	echo -e "\tpull_full <ruta al directorio con repositorios>"
+	exit
 
-for REP in $REP_LIST
-do
-	echo "Descargando cambios en [$REP]"
-	cd "$HOME/Repositorios/$REP"
-	git pull
-	echo
-done
+fi
+
+if [[ -d "$1" ]]
+then
+	REP_LIST=$(ls "$1")
+
+	for REP in $REP_LIST
+	do
+		echo "Descargando cambios en [$REP]"
+		cd "$HOME/Repositorios/$REP"
+		git pull
+		echo
+	done
+else
+	echo -e "No existe el directorio especificado: $1"
+	exit
+fi
