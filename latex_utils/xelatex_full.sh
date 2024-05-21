f584f8b0f2a77a27117df4f7127f46ef3509ad5b
Juan Manuel Espinosa Moral <juan.espinosa@eb2.tech>
2024-05-21 16:17:07 +0200

    Correciones de LaTeX

--- latex_utils/xelatex_full.sh
+++ latex_utils/xelatex_full.sh
@@ -2,17 +2,21 @@
 
 # Eliminar ficheros de compiñación anteriores
 rm *.aux
+rm *.bbl
 rm *.bcf
+rm *.blg
+rm *.cut
 rm *.lof
 rm *.log
 rm *.lot
 rm *.out
+rm *.pytxcode
 rm *.run.xml
+rm *.synctex
 rm *.synctex.gz
+rm "*.synctex.gz(busy)"
 rm *.toc
 rm pdfa.xmpi
-rm *.bbl
-rm *.blg
 
 # Compilar con pdflatex
 xelatex -synctex=1 -interaction=nonstopmode *.tex
